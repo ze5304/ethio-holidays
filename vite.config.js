024bd746bbe9bd704fe0cadaf18920d3a5355b8a
ze5304 <zelekewbelay819@gmail.com>
2026-06-02 04:47:13 +0300

    chore: build site to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ethio-holidays/',
+   base: 'https://ze5304.github.io/ethio-holidays/',
   build: {
     outDir: 'docs',
     emptyOutDir: true
